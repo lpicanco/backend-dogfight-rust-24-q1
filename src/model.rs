5b815df95839ad7c3990e697e8c1bb03aa6dde27
lpicanco <lpicanco@gmail.com>
2024-02-06 22:17:43 +0000

    feat: add model

---
+++ src/model.rs
@@ -0,0 +1,33 @@
+use serde_derive::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Debug, Validate, Deserialize, sqlx::FromRow, Serialize)]
+pub struct Transaction {
+    #[validate(required, range(min = 1))]
+    pub valor: Option<f32>,
+    #[validate(required, length(min = 1, max = 1))]
+    pub tipo: Option<String>,
+    #[validate(required, length(min = 1, max = 10))]
+    pub descricao: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct AccountStatement {
+    pub saldo: AccountBalance,
+    pub ultimas_transacoes: Vec<TransactionDetail>,
+}
+
+#[derive(Serialize)]
+pub struct AccountBalance {
+    pub total: i32,
+    pub data_extrato: String,
+    pub limite: i32,
+}
+
+#[derive(Serialize, sqlx::FromRow)]
+pub struct TransactionDetail {
+    valor: i32,
+    tipo: String,
+    descricao: String,
+    realizada_em: String,
+}
